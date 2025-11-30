ff5fa5411dcf687411a3c36626f1989b29d5ec78
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-11-30 16:02:33 -0600

    added dependency and tweaked eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -18,15 +18,22 @@ export default defineConfig([
 		// your overrides
 	},
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
   // { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   // { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  tseslint.configs.recommended,
+  tseslint.config({
+    rules: {
+      "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+    }
+  }),
   ...eslintPluginAstro.configs.recommended,
   {
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
+
     }
   },
   eslintConfigPrettier,
